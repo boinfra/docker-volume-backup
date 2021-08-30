55519ac6635bb4f041a25d0f46863226102482db
boif <franck.boinay@ruag.com>
2021-08-30 16:35:13 +0200

    Update

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -44,6 +44,7 @@ fi
 # Add our cron entry, and direct stdout & stderr to Docker commands stdout
 echo "Installing cron.d entry: docker-volume-backup"
 echo "$BACKUP_CRON_EXPRESSION root /root/backup.sh > /proc/1/fd/1 2>&1" > /etc/cron.d/docker-volume-backup
+echo "$BACKUP_CRON_EXPRESSION root /root/remove.sh > /proc/1/fd/1 2>&1" > /etc/cron.d/docker-volume-backup
 
 # Let cron take the wheel
 echo "Starting cron in foreground with expression: $BACKUP_CRON_EXPRESSION"
